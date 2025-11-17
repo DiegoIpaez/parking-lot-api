9c6fa993a5e6e1d1dddf290078fcd1396619c491
Diego Iván Paez <82846493+DiegoIpaez@users.noreply.github.com>
2025-11-17 18:12:02 -0300

    fix: the not parket filter is added (#4)

--- src/modules/vehicles/dto/find-vehicle.dto.ts
+++ src/modules/vehicles/dto/find-vehicle.dto.ts
@@ -1,8 +1,15 @@
+import { Type } from 'class-transformer';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { PartialType, IntersectionType } from '@nestjs/mapped-types';
-import { CreateVehicleDto } from './create-vehicle.dto';
 import { PaginationDto } from '@/utils/pagination/dto/pagination.dto';
+import { CreateVehicleDto } from './create-vehicle.dto';
 
 export class FindVehiclesDto extends IntersectionType(
   PaginationDto,
   PartialType(CreateVehicleDto)
-) {}
+) {
+  @Type(() => Boolean)
+  @IsBoolean()
+  @IsOptional()
+  notParked?: boolean;
+}
