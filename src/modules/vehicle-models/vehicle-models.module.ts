4e4116f597bef6da14ff2d62ceb77e36616f6fdf
DiegoIpaez <diegoipaez2000@gmail.com>
2025-12-12 15:04:33 -0300

    feat: the module to obtain all vehicle models is added

---
+++ src/modules/vehicle-models/vehicle-models.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { VehicleModelsService } from './vehicle-models.service';
+import { VehicleModelsController } from './vehicle-models.controller';
+
+@Module({
+  controllers: [VehicleModelsController],
+  providers: [VehicleModelsService],
+  exports: [VehicleModelsService],
+})
+export class VehicleModelsModule {}
